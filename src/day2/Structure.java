41a5364e089bc7807c59b69b07f2586eafc7782d
sridmtech <sridmtech@gmail.com>
2023-05-27 19:53:32 +0530

    Update Day2

---
+++ src/day2/Structure.java
@@ -0,0 +1,14 @@
+//Documentation Section
+package day2;
+//import statement
+//interface section
+
+//class definition
+public class Structure {
+	//main method definition 	
+	public static void main(String[] args) {
+		
+		//Body of main method
+	}
+
+}
