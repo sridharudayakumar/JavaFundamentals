b0f6bd364d49221c68736c4353758eade81ebd6c
sridmtech <sridmtech@gmail.com>
2023-05-27 21:43:24 +0530

    Day 3 Update

--- src/day2/Structure.java
+++ src/day2/Structure.java
@@ -7,7 +7,7 @@ package day2;
 public class Structure {
 	//main method definition 	
 	public static void main(String[] args) {
-		
+		System.out.println("Sample Application");
 		//Body of main method
 	}
 
---
+++ src/day3/DataTypeDemo.java
@@ -0,0 +1,42 @@
+package day3;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		//byte
+		byte myByteNum = 10;
+		System.out.println("Byte value:"+myByteNum);
+		
+		//short
+		short myShortNum = 100;
+		System.out.println("Short value:"+myShortNum);
+		
+		//integer
+		int myIntNum = 30000;
+		System.out.println("Integer value:"+myIntNum);
+		
+		//long 
+		long myLongNum = 18600000;
+		System.out.println("Long value:"+myLongNum);
+
+		
+		//float type
+		float myFloatNum = 5.99f;
+		System.out.println("Float value:"+myFloatNum);
+		
+		//double
+		double myDoubleNum = 3.1416;
+		System.out.println("Double value:"+myDoubleNum);
+		
+		//char Type
+		char myLetter='A';
+		System.out.println("Character value:"+myLetter);
+		
+		//boolean type
+		boolean myBool = true;
+		
+		System.out.println("Boolean value:"+myBool);
+		
+	}
+
+}
